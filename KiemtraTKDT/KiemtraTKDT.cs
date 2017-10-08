46b155abcc5e8819940a7d3d7cae3acee24b27c9
linh0893 <linh0893@gmail.com>
2017-10-08 22:57:35 +0700

    add ICD kiem tra tai khoan doanh thu

---
+++ KiemtraTKDT/KiemtraTKDT.cs
@@ -0,0 +1,74 @@
+﻿using CDTLib;
+using DevExpress.XtraEditors;
+using Plugins;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KiemtraTKDT
+{
+    public class KiemtraTKDT : ICData
+    {
+
+        private InfoCustomData info = new InfoCustomData(IDataType.Single);
+        private DataCustomData data;
+        DataRow drMaster;
+        public DataCustomData Data
+        {
+            set { data = value; }
+        }
+
+        public InfoCustomData Info
+        {
+            get { return info; }
+        }
+
+        public void ExecuteAfter()
+        {
+        }
+
+        public void ExecuteBefore()
+        {
+            if (drMaster.RowState == DataRowState.Deleted)
+                return;
+            if (data.CurMasterIndex < 0)
+                return;
+
+            checkTKDT();
+        }
+
+        private void checkTKDT()
+        {
+            drMaster = data.DsData.Tables[0].Rows[data.CurMasterIndex];
+            DateTime ngayct = DateTime.Parse(drMaster["NgayCT", DataRowVersion.Current].ToString());
+
+            string pk = data.DrTableMaster["Pk"].ToString();
+            DataRow[] drDetail = data.DsData.Tables[1].Select(string.Format("{0} = '{1}'", pk, drMaster[pk]));
+
+            foreach (DataRow row in drDetail)
+            {
+                int thang = Convert.ToInt32(drMaster["SoKy"].ToString());
+                if (!string.IsNullOrEmpty(drMaster["TGPB", DataRowVersion.Current].ToString()))
+                {
+                    DateTime ngayPb = DateTime.Parse(drMaster["TGPB", DataRowVersion.Current].ToString());
+                    if ((thang <= 1) || (ngayPb.Year <= ngayct.Year && ngayPb.Month <= ngayct.Month))
+                    {
+                        XtraMessageBox.Show("Bắt buộc nhập tài khoản doanh thu", Config.GetValue("PackageName").ToString(), MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        info.Result = false;
+                        return;
+                    }
+                } else
+                {
+                    if ((thang <= 1))
+                    {
+                        XtraMessageBox.Show("Bắt buộc nhập tài khoản doanh thu", Config.GetValue("PackageName").ToString(), MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        info.Result = false;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
